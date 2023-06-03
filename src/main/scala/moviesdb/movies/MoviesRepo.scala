d929339c92b8fa4be1f99869fbb866bcb8a78bee
Dawid Łakomy <dawid.lakomy1@gmail.com>
2023-06-03 19:57:05 +0200

    Implement standalone movie creation and retrieval

--- src/main/scala/moviesdb/movies/MoviesRepo.scala
+++ src/main/scala/moviesdb/movies/MoviesRepo.scala
@@ -11,6 +11,7 @@ import doobie.util.query.Query0
 import doobie.util.update.Update0
 import doobie.{ConnectionIO, Read, Transactor, Update}
 import moviesdb.core.syntax.MoviesSyntax.*
+import moviesdb.domain.DbError.InvalidData
 import moviesdb.domain.{PasswordHash, User, UserName}
 import moviesdb.movies.{DbErrorOr, MoviesRepoAlgebra}
 
@@ -22,13 +23,26 @@ class MoviesRepo[F[_]: MonadCancelThrow: UUIDGen](xa: Transactor[F]) extends Mov
   def getMoviesForUser(id: UserId): F[List[Movie]] =
     val transaction = for
       standalones <- getStandalonesForUserQry(id).to[List]
-      series <- getSeriesForUserQry(id).to[List]
+      series <- getAllSeriesForUserQry(id).to[List]
     yield standalones ++ series
 
     transaction.transact(xa)
 
-  def getMovie(movieId: MovieId, userId: UserId): F[Option[Movie]] = ???
-  def createMovie(movie: NewMovie, userId: UserId): F[DbErrorOr[Movie]] = ???
+  def getMovie(movieId: MovieId, userId: UserId): F[Option[Movie]] =
+    getStandaloneForUserQry(movieId, userId).option.transact(xa).flatMap {
+      case None => None.pure // later: getSeriesForUserQry(movieId, userId).option.transact(xa).widen
+      case a@Some(std) => a.pure
+    }
+
+  def createMovie(movie: NewMovie, userId: UserId): F[DbErrorOr[Movie]] = movie match
+    case movie: NewStandalone =>
+      for
+        newId <- UUIDGen.randomUUID.map(MovieId.apply)
+        _ <- insertStandalone(movie.withId(newId), userId).run.transact(xa)
+        newMovie <- getMovie(newId, userId)
+      yield newMovie.toRight(DbError.MovieNotFound) // imprecise, but let it be
+    case _ => ???
+
   def deleteMovie(movieId: MovieId, userId: UserId): F[Option[Unit]] = ???
   def updateMovie(updatedMovie: Movie, userId: UserId): F[DbErrorOr[Unit]] = ???
 
@@ -37,9 +51,15 @@ private[this] object MoviesQueries:
     sql"SELECT id, title, year FROM standalones WHERE owner_id = $userId"
       .query[Standalone]
 
-  def getSeriesForUserQry(userId: UserId): Query0[Series] = ???
+  def getStandaloneForUserQry(movieId: MovieId, userId: UserId): Query0[Standalone] =
+    sql"SELECT id, title, year FROM standalones WHERE owner_id = $userId and id = $movieId"
+      .query[Standalone]
 
-  def insertMovie(movie: Standalone, userId: UserId): Update0 =
+  def getAllSeriesForUserQry(userId: UserId): Query0[Series] = ???
+
+  def getSeriesForUserQry(movieId: MovieId, userId: UserId): Query0[Series] = ???
+
+  def insertStandalone(movie: Standalone, userId: UserId): Update0 =
     sql"""
       INSERT INTO standalones (id, title, year, owner_id)
       VALUES (${movie.id}, ${movie.title}, ${movie.year}, $userId)
--- src/test/scala/moviesdb/MoviesRepoSpec.scala
+++ src/test/scala/moviesdb/MoviesRepoSpec.scala
@@ -2,6 +2,7 @@ package moviesdb.movies.sqlite
 
 import cats.effect.unsafe.implicits.global
 import cats.effect.{IO, Resource}
+import cats.syntax.all.*
 import doobie.implicits.*
 import doobie.{munit as _, *}
 import moviesdb.core.syntax.MoviesSyntax.*
@@ -16,18 +17,25 @@ import java.util.UUID
 class MoviesRepoSpec extends munit.FunSuite with doobie.munit.IOChecker:
 
   private val uid = UserId(UUID.randomUUID())
+  private val otherUid = UserId(UUID.randomUUID())
 
   val ds = dataSourceFromConnString(inMemoryConnString("moviesRepo"))
   val transactor: Transactor[IO] = Transactor.fromConnection(ds.getConnection)
 
-  override def beforeAll(): Unit = initDb[IO](ds).unsafeRunSync()
-  // TODO add a user
+  override def beforeAll(): Unit =
+    initDb[IO](ds).unsafeRunSync()
+    sql"insert into users(id, name, password_hash) values ($uid, 'testuser', 'whatever')"
+      .update.run.transact(transactor).unsafeRunSync()
 
   private val moviesRepo = MoviesRepo(transactor)
 
   test("The DMLs should typecheck") {
     import MoviesQueries.*
-    check(getStandalonesForUserQry(UserId(UUID.randomUUID())))
+
+    val randomUUID = UUID.randomUUID()
+
+    check(getStandalonesForUserQry(uid))
+    check(getStandaloneForUserQry(mid1, uid))
 
     // fails, I suspect https://github.com/tpolecat/doobie/issues/1782
     // check(insertMovie(standaloneTemplate.withId(mid1), uid1))
@@ -36,19 +44,17 @@ class MoviesRepoSpec extends munit.FunSuite with doobie.munit.IOChecker:
   test("Should create and retrieve a standalone movie") {
 
     val program = for
-      idFromCreate <- moviesRepo.createMovie(standaloneTemplate, uid).map(_.map(_.id))
-      movieFromRepo <- idFromCreate match
-        case Left(err) => IO(fail("Movie creation failed: "+err.toString))
-        case Right(id) =>
-          moviesRepo.getMovie(id, uid)
-    // get is safe below; see `fail` above
-    yield (idFromCreate.toOption.get, movieFromRepo)
+      maybeIdFromCreate <- moviesRepo.createMovie(standaloneTemplate, uid).map(_.map(_.id))
+      id <- maybeIdFromCreate match {
+        case Left(err) => IO(fail("Movie creation failed: " + err.toString))
+        case Right(id) => id.pure
+      }
+      movieFromRepo <- moviesRepo.getMovie(id, uid)
+      otherUserMovie <- moviesRepo.getMovie(id, otherUid)
+    yield (id, movieFromRepo, otherUserMovie)
 
-    val (newId, movieFromRepo) = program.unsafeRunSync()
+    val (newId, movieFromRepo, otherUserMovie) = program.unsafeRunSync()
 
-    assertEquals(movieFromRepo, Some(standaloneTemplate.withId(newId)))
-  }
-
-  test("Should fail to retrieve a movie when user id is mismatched".ignore) {
-    ???
+    assertEquals(movieFromRepo, Some(standaloneTemplate.withId(newId)), "Movie not found")
+    assertEquals(otherUserMovie, None, "Movie shouldn't be found with a wrong userId")
   }
